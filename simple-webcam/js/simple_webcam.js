1e5fc0fa1307c54b81ce63bc03bc5ae2157a8e81
Lydzje <to.lydzje@gmail.com>
2017-10-02 22:56:40 +0200

    Initial commit
    Initial commit with a simple webcam application

---
+++ simple-webcam/js/simple_webcam.js
@@ -0,0 +1,30 @@
+'user strict';
+
+// VARIABLES
+// ---------
+// Variables are in global scope so we can look at them through debuggers
+var video = document.getElementById('video');
+
+var windowUrl = window.URL || window.webkitURL;
+
+navigator.getMedia = navigator.getUserMedia ||
+  navigator.mozGetUserMedia ||
+  navigator.webkitGetUserMedia ||
+  navigator.msGetUserMedia;
+
+var constraints = {
+  video: true
+};
+
+// getUserMedia CALLBACKS
+// ----------------------
+function successCallback(stream) {
+  console.log("Getting the media stream succeded!", stream);
+  video.objectSrc = windowUrl.createObjectURL(stream);
+}
+
+function errorCallback(error) {
+  console.log("Getting the media stream failed: ", error);
+}
+
+navigator.getMedia(constraints, successCallback, errorCallback);
